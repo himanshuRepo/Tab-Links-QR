edb5b77b2729debe857c28de487dd02cbb2ff510
Himanshu Mittal <himanshu.mittal224@gmail.com>
2018-02-26 00:52:54 +0530

    Add files via upload

---
+++ createQR.py
@@ -0,0 +1,27 @@
+'''
+Tab Link QR Generator
+Purpose: to open all the tabs opened in broswer on another browser using QR scanner
+
+File: Generate the QR code as image using first 10 links
+'''
+
+import pyqrcode
+import os
+
+a1=''
+count=0
+noLinks=10
+file1 = open("data.txt","r+") 
+for i in file1.readlines():
+	a1+=i[:-1]
+	if count < noLinks:
+		count+=1
+	else:
+		break
+print a1
+
+
+u1 = unicode(a1, "utf-8")
+img = pyqrcode.create(u1)
+
+img.png('img1.png', scale=5)
---
+++ tabLinks.py
@@ -0,0 +1,23 @@
+'''
+Tab Link QR Generator
+Purpose: to open all the tabs opened in broswer on another browser using QR scanner
+
+File: Storing current links in file 'data.txt'
+'''
+
+
+import re
+
+a1=''
+pathToCurrentTabs="/Users/..../Library/Application Support/Google/Chrome/Profile 1/Current Tabs"
+file1 = open(pathToCurrentTabs,"r") 
+g1=file1.read()
+da = g1.split("\x00")
+pattern = re.compile(r'https?://(.*?)x0')
+pn = re.compile(r'https?://*')
+nw = filter(pn.match, da)
+st1 = '$\n'.join(nw)
+st1 = st1+'$\n'
+f = open('data.txt','w')
+f.write(st1)
+f.close()
